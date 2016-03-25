b56caed550281ef0f1ec7c9e7b4c8942edbd4d13
AdamDove <Adambdove+github@gmail.com>
2016-03-24 20:43:55 -0600

    Add View and ViewModel for Notify Button.  Very basic right now, and doesn't do anything yet.

---
+++ Overlay/ViewModel/AppControlViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overlay.ViewModel
+{
+    public delegate void ApplicationExitEvent(object sender, EventArgs e);
+
+    public class AppControlViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event ApplicationExitEvent OnApplicationExit;
+
+    }
+}
