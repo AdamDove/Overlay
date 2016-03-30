fa6ee6356d2b6e555075f9c6381dee69a31ae834
AdamDove <Adambdove+github@gmail.com>
2016-03-29 22:21:24 -0600

    Create CommandDelegate to handle Button Clicks and provide implementation for Exit button press on AppControl Window.

---
+++ Overlay/Common/CommandDelegate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Overlay.Common
+{
+    public class CommandDelegate : ICommand
+    {
+        private Action m_action;
+        private bool m_canExecute;
+            
+        public event EventHandler CanExecuteChanged;
+
+        public CommandDelegate(Action action, bool canExecute)
+        {
+            m_action = action;
+            m_canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return m_canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            m_action();
+        }
+    }
+}
--- Overlay/ViewModel/AppControlViewModel.cs
+++ Overlay/ViewModel/AppControlViewModel.cs
@@ -1,19 +1,27 @@
-﻿using System;
+﻿using Overlay.Common;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Overlay.ViewModel
 {
-    public delegate void ApplicationExitEvent(object sender, EventArgs e);
-
     public class AppControlViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public event ApplicationExitEvent OnApplicationExit;
+        public ICommand OnExitClick
+        {
+            get { return new CommandDelegate(ExitApplication, true); }
+        }
 
+        private void ExitApplication()
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
